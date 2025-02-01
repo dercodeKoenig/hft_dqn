52657e7c82e122139e8c83bdf455b28113efc9b4
Marvin Eckhardt <85019164+dercodeKoenig@users.noreply.github.com>
2025-02-01 03:48:54 +0100

    Create tts_server_worker.py

---
+++ tts_server_worker.py
@@ -0,0 +1,145 @@
+import os
+import pymysql
+from io import BytesIO
+from ftplib import FTP
+import random
+import json
+from tqdm import tqdm
+import numpy as np
+import io
+
+import time
+start_time = time.time()
+
+# FTP details to upload the embeddings
+FTP_HOST = 'benundmarvpromotions.lima-ftp.de'
+FTP_USER = 'benundmarvpromotions'
+FTP_PASS = 'gWEhrtjanrgy'
+REMOTE_DIR = '/online_tts/data/'
+
+# Database connection details
+DB_HOST = "benundmarvpromotions.lima-db.de"
+DB_USER = "USER433859_2345"
+DB_PASS = "Sonne-12345"
+DB_NAME = "db_433859_6"
+
+
+voices = [
+    "af_bella",
+    "bf_isabella",
+    "bm_lewis",
+    "am_adam",
+    "am_michael",
+    "af_nicole",
+    "af_sarah",
+    "af_sky",
+    "bf_emma",
+    "bm_george",
+]
+
+
+
+# 3️⃣ Initalize a pipeline
+from kokoro import KPipeline
+from IPython.display import display, Audio
+import soundfile as sf
+
+
+pipelines = {}
+
+def process(text, lang = "a", voice = "bf_isabella", speed = 1.1):
+
+    if lang in pipelines:
+        pipeline = pipelines[lang]
+    else:
+        pipe_t0 = time.time()
+        pipeline = KPipeline(lang_code=lang) # <= make sure lang_code matches voice
+        pipelines[lang] = pipeline
+        print("created pipeline for", lang, "took", time.time()-pipe_t0, "seconds")
+    
+    
+    generator = pipeline(
+        text, voice=voice, # <= change voice here
+        speed=speed
+    )
+    
+    all_audio = []  # List to store all audio data
+    
+    for i, (gs, ps, audio) in enumerate(generator):
+        #print(i)  # Index
+        #print(gs) # Text
+        #print(ps) # Phonemes
+        all_audio.append(audio)  # Collect audio segments
+    
+    # Concatenate all audio segments into one
+    combined_audio = np.concatenate(all_audio)
+    
+    return combined_audio
+
+
+pwr = 0
+
+while True:
+    
+    connection = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASS, database=DB_NAME)
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM `requests` WHERE output_path=''")
+    data = [x for x in cursor.fetchall()]
+    connection.close()
+    
+    if(len(data) == 0):
+        if(pwr == 1):
+            os.system("powercfg /S faf11aa6-9bb4-4f50-98e1-d401f3368cfd")
+            pwr = 0
+        time.sleep(1)
+    else:
+        if(pwr == 0):
+            os.system("powercfg /S 381b4222-f694-41f0-9685-ff5bb260df2e")
+            pwr = 1
+        
+        try:
+            data = data[0]
+            print(data)
+    
+            text = data[2]
+    
+            voice = "bf_isabella"
+            if data[5] in voices:
+                voice = data[5]
+    
+            speed = data[6]
+            speed = min(4,max(0.25,speed))
+
+            process_t0 = time.time()
+            
+            audio = process(text, voice = voice, lang = 'a' if voice[0] == 'a' else 'b', speed = speed)
+
+            print("processing took", time.time()-process_t0, "seconds")
+            # Save as a single file
+             # Create a BytesIO buffer to hold the audio data in memory
+            audio_buffer = io.BytesIO()
+            
+            # Write audio to the buffer instead of a file
+            sf.write(audio_buffer, audio, 24000,  format='WAV')
+            audio_buffer.seek(0)  # Rewind the buffer to the beginning
+            
+            # FTP upload
+            output_name = data[0] + "_" + str(data[1]) + ".wav"
+            
+            ftp = FTP(FTP_HOST)
+            ftp.login(FTP_USER, FTP_PASS)
+            ftp.storbinary(f'STOR {REMOTE_DIR}/{output_name}', audio_buffer)
+            ftp.quit()
+                
+        except Exception as e:
+            print(e)
+            output_name = "_error_"
+
+        connection = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASS, database=DB_NAME)
+        cursor = connection.cursor()
+        cursor.execute(
+            "UPDATE `requests` SET output_path = %s, time = NOW() WHERE user = %s AND req_id = %s",
+            (output_name, data[0], data[1])
+        )
+        connection.commit()
+        connection.close()
